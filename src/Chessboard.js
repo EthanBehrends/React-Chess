fb7e4d61fb1141bcc1b57aab5eca354f4d468baf
Ethan Behrends <epbehrends@gmail.com>
2021-05-06 16:36:12 -0500

    Move Piece function
    Can now move pieces within the scope of the chessboard

--- src/Chessboard.js
+++ src/Chessboard.js
@@ -22,10 +22,13 @@ function keyToArray(key) {
 }
 
 function Chessboard() {
+    function useForceUpdate(){
+        const [value, setValue] = useState(0); // integer state
+        return () => setValue(value => value + 1); // update the state to force render
+    }
 
 
-
-    function getPiece(board, key) {
+    function getPiece(key) {
         console.log(keyToArray(key)[0] + " " + keyToArray(key)[1]);
         return (board[keyToArray(key)[0]][keyToArray(key)[1]]);
     }
@@ -41,23 +44,37 @@ function Chessboard() {
         let tempboard = board;
         tempboard[keyToArray(fCoord)[0]][keyToArray(fCoord)[1]] = tempboard[keyToArray(iCoord)[0]][keyToArray(iCoord)[1]];
         tempboard[keyToArray(iCoord)[0]][keyToArray(iCoord)[1]] = '.';
-        return tempboard;
+        setBoard(tempboard);
+        forceUpdate();
     }
 
+    const forceUpdate = useForceUpdate();
 
     const [board, setBoard] = useState(defaultBoard);
     const [whitePOV, setWhitePOV] = useState(true);
 
     return(
         <div id="board" className={whitePOV ? 'white' : 'black'}>
-            {board.map((row, i) => {
-                return(row.map((cell, j) => {
-                    return(<Tile key={charToInt(8-i) + (j+1)} label={charToInt(8-i) + (j+1)} piece={board[7-j][7-i]} color={(i + j) % 2 === 1 ? 'dark' : 'light'}></Tile>);
-                }))
-            })}
+            {
+                board.map((row, i) => {
+                    return(row.map((cell, j) => {
+                        return(<Tile key={charToInt(8-i) + (j+1)} label={charToInt(8-i) + (j+1)} piece={board[7-j][7-i]} color={(i + j) % 2 === 1 ? 'dark' : 'light'}></Tile>);
+                    }))
+                })
+            }
             <div id="POVFlip" onClick={() => {setWhitePOV(!whitePOV)}}></div>
-            <div id="testButton" onClick={() => {setBoard(move('E2', 'E4'))}}></div>
+            <div id="testButton" onClick={() => {move('A1', 'F4')}}></div>
         </div>
+    )
+}
+
+function Grid(props) {
+    return (
+        props.board.map((row, i) => {
+            return(row.map((cell, j) => {
+                return(<Tile key={charToInt(8-i) + (j+1)} label={charToInt(8-i) + (j+1)} piece={props.board[7-j][7-i]} color={(i + j) % 2 === 1 ? 'dark' : 'light'}></Tile>);
+            }))
+        })
     )
 }
 
