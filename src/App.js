339517c869e75ef4618c58090892b64e35479c02
Ethan Behrends <epbehrends@gmail.com>
2021-06-28 18:15:22 -0500

    Revert "About Panel prototype"
    This reverts commit 80067d978dd5386c167b746037efadb4625c1056.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react'
 import Chessboard from './Chessboard.js'
 import Connector from './Connector.js'
 import TurnMarker from './TurnMarker.js'
-import AboutPanel from './AboutPanel.js'
 import io from 'socket.io-client'
 
 
@@ -61,7 +60,6 @@ function App() {
       <Chessboard isWhite={isWhite} setIsTurn={setIsTurn} setIsActive={setIsActive} setIsWhite={setIsWhite} isHost={isHost} room={room} socket={socket} />
       <Connector joinRoom={joinRoom} createRoom={createRoom} unhost={unhost} room={room} socket={socket} />
       <TurnMarker isTurn={isTurn} isActive={isActive} />
-      <AboutPanel />
     </div>
   );
 }
