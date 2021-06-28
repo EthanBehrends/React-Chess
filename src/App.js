6586291e8e1bbdc25b150af10004004e9c72d4e0
Ethan Behrends <epbehrends@gmail.com>
2021-06-28 18:35:05 -0500

    About Panel finished

--- src/App.js
+++ src/App.js
@@ -61,7 +61,17 @@ function App() {
       <Chessboard isWhite={isWhite} setIsTurn={setIsTurn} setIsActive={setIsActive} setIsWhite={setIsWhite} isHost={isHost} room={room} socket={socket} />
       <Connector joinRoom={joinRoom} createRoom={createRoom} unhost={unhost} room={room} socket={socket} />
       <TurnMarker isTurn={isTurn} isActive={isActive} />
-      <AboutPanel />
+      <AboutPanel>
+      <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida tortor nunc, at pulvinar dui hendrerit eget. Donec luctus ex eget sem vestibulum porttitor. Duis rhoncus lorem odio, ac varius urna dapibus eu. Aliquam vitae sem condimentum, pellentesque diam in, dictum erat. Duis finibus mi ut justo consectetur, vitae consequat orci maximus. Donec posuere, justo quis egestas porta, ante turpis ornare sapien, ut volutpat diam nulla in odio. Quisque varius, ante quis porttitor lacinia, mauris magna malesuada metus, aliquet congue mauris sem in neque. Sed eget justo lorem. Curabitur sollicitudin sodales bibendum.
+                </p>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida tortor nunc, at pulvinar dui hendrerit eget. Donec luctus ex eget sem vestibulum porttitor. Duis rhoncus lorem odio, ac varius urna dapibus eu. Aliquam vitae sem condimentum, pellentesque diam in, dictum erat. Duis finibus mi ut justo consectetur, vitae consequat orci maximus. Donec posuere, justo quis egestas porta, ante turpis ornare sapien, ut volutpat diam nulla in odio. Quisque varius, ante quis porttitor lacinia, mauris magna malesuada metus, aliquet congue mauris sem in neque. Sed eget justo lorem. Curabitur sollicitudin sodales bibendum.
+                </p>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida tortor nunc, at pulvinar dui hendrerit eget. Donec luctus ex eget sem vestibulum porttitor. Duis rhoncus lorem odio, ac varius urna dapibus eu. Aliquam vitae sem condimentum, pellentesque diam in, dictum erat. Duis finibus mi ut justo consectetur, vitae consequat orci maximus. Donec posuere, justo quis egestas porta, ante turpis ornare sapien, ut volutpat diam nulla in odio. Quisque varius, ante quis porttitor lacinia, mauris magna malesuada metus, aliquet congue mauris sem in neque. Sed eget justo lorem. Curabitur sollicitudin sodales bibendum.
+                </p>
+      </AboutPanel>
     </div>
   );
 }
